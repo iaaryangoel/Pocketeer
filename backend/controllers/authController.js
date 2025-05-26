c49288316f9c65350b6d898b6c97991fbefc59f1
Aaryan Goel <aaryangoel.ag@gmail.com>
2025-05-26 19:42:04 +0530

    Handled profile image not showing bug

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,83 +1,93 @@
 const jwt = require("jsonwebtoken");
-const User = require("../model/User")
+const User = require("../model/User");
+const backendURL = process.env.BACKEND_BASE_URL;
 
 // Generate JWT token
 // Generates JWT token for a user that expires in 1 hour
 const generateToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1h"});
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
 
 // Register User
-exports.registerUser = async (req,res) => {
-    const {fullName, email, password, profileImageUrl} = req.body; // Destructure data from client
+exports.registerUser = async (req, res) => {
+  const { fullName, email, password, profileImageUrl } = req.body; // Destructure data from client
 
-    // Validation: check for missing fields
-    if(!fullName || !email || !password){
-        return res.status(400).json({message: "All fields are required."});
+  // Validation: check for missing fields
+  if (!fullName || !email || !password) {
+    return res.status(400).json({ message: "All fields are required." });
+  }
+
+  try {
+    // Check if email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use." });
     }
 
-    try{
-        // Check if email already exists
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            return res.status(400).json({message: "Email already in use."});
-        }
+    // Create the user if doesn't exist
+    const user = await User.create({
+      fullName,
+      email,
+      password,
+      profileImageUrl,
+    });
 
-        // Create the user if doesn't exist
-        const user = await User.create({
-            fullName,
-            email,
-            password,
-            profileImageUrl,
-        });
-
-        res.status(201).json({
-            id: user._id,
-            user,
-            token: generateToken(user._id),
-        });
-    } catch (err) {
-        res.status(500).json({message: "Error registering user.", error: err.message});
-    }
+    res.status(201).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error registering user.", error: err.message });
+  }
 };
 
 // Login User
-exports.loginUser = async (req,res) => {
-    const {email, password} = req.body; // Destructure data from client
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body; // Destructure data from client
 
-    // Validation: check for missing fields
-    if(!email || !password){
-        return res.status(400).json({message: "All fields are required."});
+  // Validation: check for missing fields
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required." });
+  }
+
+  try {
+    // Check credentials
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(400).json({ message: "Invalid credentials." });
     }
 
-    try{
-        // Check credentials 
-        const user = await User.findOne({email});
-        if(!user || !(await user.comparePassword(password))){
-            return res.status(400).json({message: "Invalid credentials."});
-        }
-
-        res.status(201).json({
-            id: user._id,
-            user,
-            token: generateToken(user._id),
-        });
-    } catch (err) {
-        res.status(500).json({message: "Error logging user.", error: err.message});
+    res.status(201).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error logging user.", error: err.message });
+  }
 };
-}
 
 // Get User Info
-exports.getUserInfo = async (req,res) => {
-    try{
-        const user = await User.findById(req.user.id).select("-password"); // Exclude password
+exports.getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password"); // Exclude password
 
-        if(!user){
-            return res.status(400).json({message: "User not found."})
-        }
-
-        res.status(200).json(user);
-    } catch(err){
-        res.status(500).json({message: "Error loading user.", error: err.message});
+    if (!user) {
+      return res.status(400).json({ message: "User not found." });
     }
+
+    if (user.profileImageUrl) {
+      user.profileImageUrl = `${backendURL}/${user.profileImageUrl}`;
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error loading user.", error: err.message });
+  }
 };
